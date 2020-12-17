0156faaa2b9cddc243716eb43da40866a88d2f4c
puredev2019 <puredev2019@gmail.com>
2020-12-17 11:23:15 +0700

    tuan new

--- custom/modules/AOS_Quotes/APIUpdateFilesToQuoteFromPE.php
+++ custom/modules/AOS_Quotes/APIUpdateFilesToQuoteFromPE.php
@@ -983,7 +983,7 @@ if($_POST['to_module'] == "aos_invoice"){
         $mail->Body .= "<p>Link Invoice: <a href='https://suitecrm.pure-electric.com.au/index.php?module=AOS_Invoices&action=EditView&record=".$invoice->id."' target='_blank'>".$invoice->name."</a></p>";
         $mail->Body .= "<p>Email Client: <a href='https://mail.google.com/#search/".$client->email1."'>".$client->email1." GSearch</a></p>";
     }else {
-        $mail->Subject = $worker_type." ".$installer->name." uploaded photo to Invoice#".$invoice->number."";
+        $mail->Subject = $worker_type." ".$installer->name." uploaded photo to Invoice#".$invoice->number." ".$invoice->name;
         $mail->Body = $shortcuts;
         $mail->Body .= "<p>Link Invoice: <a href='https://suitecrm.pure-electric.com.au/index.php?module=AOS_Invoices&action=EditView&record=".$invoice->id."' target='_blank'>".$invoice->name."</a></p>";
         $mail->Body .= "<p>Email ".$worker_type.": <a href='https://mail.google.com/#search/".$installer->email1."'>".$installer->email1." GSearch</a></p>";
