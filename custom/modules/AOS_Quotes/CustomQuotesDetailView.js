18853251fda79ede0b72a845be3cb9fd2ea33f90
puredev2019 <puredev2019@gmail.com>
2021-01-15 11:19:08 +0700

    tuan new

--- custom/modules/AOS_Quotes/CustomQuotesDetailView.js
+++ custom/modules/AOS_Quotes/CustomQuotesDetailView.js
@@ -488,19 +488,21 @@ $(function () {
         var record = encodeURIComponent($("input[name='record']").val());
         $('#convert_to_invoice_button').attr('onclick',`$(document).convert_to_invoice("${record}")`);
         $.fn.convert_to_invoice = function (id_quote) {
-            if ($('div[field="proposed_install_date_c"]').text().trim() == '') {
+            if ($('#quote_type_c').val() == ""){
+                alert("Field Product Type is not filled !");
+                return false;     
+            }
+            if ($('#quote_type_c').val() == 'quote_type_sanden' && $('div[field="proposed_install_date_c"]').text().trim() == '' && $('#name').text().trim().toLowerCase().includes("supply only") != true) {
                 var question = confirm("Field Proposed Install Date is not filled - are you sure to continue?");
                 if (!question) {
                     return false;
                 }
-            }
-            if ($('#quote_type_c').val() == 'quote_type_sanden' && $('div[field="proposed_dispatch_date_c"]').text().trim() == '') {
+            }else if ($('#quote_type_c').val() == 'quote_type_sanden' && $('div[field="proposed_dispatch_date_c"]').text().trim() == '' && $('#name').text().trim().toLowerCase().includes("supply only") == true) {
                 var question = confirm("Field Proposed Dispatch Date is not filled - are you sure to continue?");
                 if (!question) {
                     return false;
                 }
-            }
-            if ($('#quote_type_c').val() == 'quote_type_daikin' && $('div[field="proposed_delivery_date_c"]').text().trim() == '') {
+            }else if ($('#quote_type_c').val() == 'quote_type_daikin' && $('div[field="proposed_delivery_date_c"]').text().trim() == '') {
                 var question = confirm("Field Proposed Delivery Date is not filled - are you sure to continue?");
                 if (!question) {
                     return false;
