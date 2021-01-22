c80345efefde33a175bc2f63d9aad321213bf2de
pureDev2019 <pureDev2019@gmail.com>
2021-01-22 08:47:42 +0700

    fix

--- custom/modules/PO_purchase_order/logic_hooks_class.php
+++ custom/modules/PO_purchase_order/logic_hooks_class.php
@@ -12,45 +12,44 @@
             $method = '';
             if($old_fields == false || empty($bean->xero_po_id_c)){
                 $method = 'create';
-            }else{
-                $method = 'update';
+            
+                $tmpfsuitename = dirname(__FILE__).'/cookiesuitecrm.txt';
+                $fields = array();
+                $fields['user_name'] = 'admin';
+                $fields['username_password'] = 'pureandtrue2020*';
+                $fields['module'] = 'Users';
+                $fields['action'] = 'Authenticate';
+                $url = 'https://suitecrm.pure-electric.com.au/index.php';
+                //$url = 'http://loc.suitecrm.com/index.php';
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($fields));
+                curl_setopt($curl, CURLOPT_POST, 1);//count($fields)
+
+                curl_setopt($curl, CURLOPT_COOKIEJAR, $tmpfsuitename);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+                curl_setopt($curl, CURLOPT_COOKIEFILE, $tmpfsuitename);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
+                curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
+                $result = curl_exec($curl);
+
+                curl_setopt($curl, CURLOPT_URL, 'https://suitecrm.pure-electric.com.au/index.php?entryPoint=xeroAPI&type=PurchaseOrder&method='.$method.'&record='.$bean->id);
+                //curl_setopt($curl, CURLOPT_URL, 'http://loc.suitecrm.com/index.php?entryPoint=xeroAPI&type=PurchaseOrder&method='.$method.'&record='.$bean->id);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+                curl_setopt($curl, CURLOPT_COOKIEJAR, $tmpfsuitename);
+                curl_setopt($curl, CURLOPT_COOKIEFILE, $tmpfsuitename);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
+                curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
+                $result = curl_exec($curl);
+                curl_close($curl);
             }
-            $tmpfsuitename = dirname(__FILE__).'/cookiesuitecrm.txt';
-            $fields = array();
-            $fields['user_name'] = 'admin';
-            $fields['username_password'] = 'pureandtrue2020*';
-            $fields['module'] = 'Users';
-            $fields['action'] = 'Authenticate';
-            $url = 'https://suitecrm.pure-electric.com.au/index.php';
-            //$url = 'http://loc.suitecrm.com/index.php';
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($fields));
-            curl_setopt($curl, CURLOPT_POST, 1);//count($fields)
-
-            curl_setopt($curl, CURLOPT_COOKIEJAR, $tmpfsuitename);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($curl, CURLOPT_COOKIEFILE, $tmpfsuitename);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
-            curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
-            $result = curl_exec($curl);
-
-            curl_setopt($curl, CURLOPT_URL, 'https://suitecrm.pure-electric.com.au/index.php?entryPoint=xeroAPI&type=PurchaseOrder&method='.$method.'&record='.$bean->id);
-            //curl_setopt($curl, CURLOPT_URL, 'http://loc.suitecrm.com/index.php?entryPoint=xeroAPI&type=PurchaseOrder&method='.$method.'&record='.$bean->id);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($curl, CURLOPT_COOKIEJAR, $tmpfsuitename);
-            curl_setopt($curl, CURLOPT_COOKIEFILE, $tmpfsuitename);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
-            curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
-            $result = curl_exec($curl);
-            curl_close($curl);
         }
     }
     class CreateInternalNotesPO {
