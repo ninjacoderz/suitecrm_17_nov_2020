bdb7a5af5c59e589281f424d4faab3cbf22bd1a2
Binh Nguyen Thanh <binhdigipro@gmail.com>
2021-01-22 12:04:46 +0700

    fix

--- custom/modules/PO_purchase_order/logic_hooks_class.php
+++ custom/modules/PO_purchase_order/logic_hooks_class.php
@@ -29,19 +29,19 @@
             curl_setopt($curl, CURLOPT_POST, 1);//count($fields)
 
             curl_setopt($curl, CURLOPT_COOKIEJAR, $tmpfsuitename);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
             curl_setopt($curl, CURLOPT_COOKIEFILE, $tmpfsuitename);
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
-            curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1);
+            //curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1);
             curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
             $result = curl_exec($curl);
 
             curl_setopt($curl, CURLOPT_URL, 'https://suitecrm.pure-electric.com.au/index.php?entryPoint=xeroAPI&type=PurchaseOrder&method='.$method.'&record='.$bean->id);
             //curl_setopt($curl, CURLOPT_URL, 'http://loc.suitecrm.com/index.php?entryPoint=xeroAPI&type=PurchaseOrder&method='.$method.'&record='.$bean->id);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
             curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
             curl_setopt($curl, CURLOPT_COOKIEJAR, $tmpfsuitename);
             curl_setopt($curl, CURLOPT_COOKIEFILE, $tmpfsuitename);
@@ -49,7 +49,7 @@
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
-            curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1);
+            //curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1);
             curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
             $result = curl_exec($curl);
             curl_close($curl);
