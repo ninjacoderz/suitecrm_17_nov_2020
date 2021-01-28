c84c28dc2cff084c6ea7f77552a33f5b5e91087d
Nguyen Phu Dung <nguyenphudung93.dn@gmail.com>
2021-01-28 08:30:36 +0700

    fix task Paul- fix custom code

--- modules/AOS_PDF_Templates/generatePdf.php
+++ modules/AOS_PDF_Templates/generatePdf.php
@@ -179,8 +179,8 @@ if($_REQUEST['module'] == 'AOS_Invoices') {
 if($custom_due_amount == 0 && $custom_total_payment != 0){
     $name = explode(" ",$bean->billing_contact);
     $short_description_c = "Dear ".$name[0].",
-
-    Acknowledge payment receipt thank you, please find your \$product_type invoice attached".(($bean->invoice_type_c!=="Methven")?" and certificates of compliance attached":"").". Congratulations for choosing the \$product_type, the most efficient in Australia and thank you for choosing PureElectric.";
+Customer Notes: \$aos_invoices_invoice_note_c
+Acknowledge payment receipt thank you, please find your \$product_type invoice attached".(($bean->invoice_type_c!=="Methven")?" and certificates of compliance attached":"").". Congratulations for choosing the \$product_type, the most efficient in Australia and thank you for choosing PureElectric.";
     
     //If you would like to, we would most grateful if you took a brief moment to leave a review, it's one way we can continue to improve and continue to bring more renewables for everyone. Here is the link if you would like to leave a Google review -> https://bit.ly/2FG751o";
 }
