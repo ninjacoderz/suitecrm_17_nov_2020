b0300734faafd098154e2eb14d694f36ef83675a
puredev2019 <puredev2019@gmail.com>
2021-02-24 15:50:15 +0700

    tuan_new

--- custom/modules/AOS_Invoices/logic_hooks.php
+++ custom/modules/AOS_Invoices/logic_hooks.php
@@ -74,11 +74,11 @@
     //     'after_save_method'
     // );
     // tuan code auto send customer warranty upload email
-    $hook_array['after_save'][] = Array(
-        6,
-        'auto_send_customer_warranty_mail',
-        'custom/modules/AOS_Invoices/logic_hooks_class.php',
-        'AutoSendCustomerWarrantyMail',
-        'after_save_AutoSendCustomerWarrantyMail'
-    );
+    // $hook_array['after_save'][] = Array(
+    //     6,
+    //     'auto_send_customer_warranty_mail',
+    //     'custom/modules/AOS_Invoices/logic_hooks_class.php',
+    //     'AutoSendCustomerWarrantyMail',
+    //     'after_save_AutoSendCustomerWarrantyMail'
+    // );
 ?>
