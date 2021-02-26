ec1f0552d707282b045d6c798b5ac73e031afcf0
puredev2019 <puredev2019@gmail.com>
2021-02-26 09:14:38 +0700

    tuan_test

--- custom/include/SugarFields/Fields/Multiupload/CustomRunCustomCode.php
+++ custom/include/SugarFields/Fields/Multiupload/CustomRunCustomCode.php
@@ -1,5 +1,123 @@
 <?php
+    $bean = new AOS_Invoices();
+    $bean->retrieve('611e5cc6-8c67-ea50-eac8-603330a8a178');
 
+    $db = DBManagerFactory::getInstance();
+    $sql = "UPDATE `emails` SET `deleted` = 1 WHERE `status` = 'email_schedule' AND `parent_id` = '$bean->id' AND `name` = 'Warranty registration photos and serials' AND deleted = 0";
+    $db->query($sql);
+    $emailTemplateID = 'a60e5ca5-6919-87ac-916c-6034cbff7477';//test 'c51e810f-f6b5-bf50-5ab6-6034cbce9ce3';
+
+    $emailtemplate = new EmailTemplate();
+    $emailtemplate = $emailtemplate->retrieve($emailTemplateID);
+
+    $contact =  new Contact();
+    $contact->retrieve($bean->billing_contact_id);
+
+    $name = $emailTemplate->subject;
+    $description_html = $emailTemplate->body_html;
+    $description = $emailTemplate->body;
+    
+    $template_data = $emailtemplate->parse_email_template(
+        array(
+            "subject" => $emailtemplate->subject,
+            "body_html" => htmlspecialchars($emailtemplate->body_html),
+            "body" => $emailtemplate->body_html
+            ),
+            'AOS_Invoices',
+            $lead,
+            $macro_nv
+        );
+    
+    $name = $template_data['subject'];
+    $description = $template_data['body'];
+    $description_html = $template_data['body_html'];
+    //parse value
+
+    $link_upload_files = 'https://pure-electric.com.au/upload_file_sanden/client-warranty?invoice_id=' . $invoice->id;
+    $string_link_upload_files = '<a target="_blank" href="'.$link_upload_files.'">Link Upload Here</a>';
+    $description = str_replace("\$contact_first_name",$contact->first_name , $description);
+    $description = str_replace("\$aos_invoices_link_upload",$string_link_upload_files , $description);
+
+    $description_html = str_replace("\$contact_first_name",$contact->first_name , $description_html);
+    $description_html = str_replace("\$aos_invoices_link_upload",$string_link_upload_files, $description_html);
+
+    $mail_From = "info@pure-electric.com.au";
+    $mail_FromName = "Pure Electric";
+    $emailSignatureId = '3ad8f82a-d3e7-5897-7c98-5ba1c4ac785e'; 
+    //signature
+    $user = new User();
+    $user->retrieve('8d159972-b7ea-8cf9-c9d2-56958d05485e');
+    $defaultEmailSignature = $user->getSignature($emailSignatureId);
+
+    if (empty($defaultEmailSignature)) {
+        $defaultEmailSignature = array(
+            'html' => '<br>',
+            'plain' => '\r\n',
+        );
+        $defaultEmailSignature['no_default_available'] = true;
+    } else {
+        $defaultEmailSignature['no_default_available'] = false;
+    }
+    $defaultEmailSignature['signature_html'] =  str_replace('Accounts', '', $defaultEmailSignature['signature_html']);
+    $description .= "<br><br><br>";
+    $description .=  $defaultEmailSignature['signature_html'];
+    $description_html .= "<br><br><br>";
+    $description_html .=  $defaultEmailSignature['signature_html'];
+    $schedule_time = strtotime(date('d-m-Y H:i:s')); //+ 24 minutes
+    //create email 
+    $email = new Email();
+    $email->id = create_guid();
+    $email->new_with_id = true;
+    $email->name = $name;
+    $email->type = "out";
+    $email->status = "email_schedule";
+    $email->parent_type = 'AOS_Invoices';
+    $email->parent_id = $bean->id;
+    $email->parent_name = $bean->name;
+    $email->mailbox_id = 'b4fc56e6-6985-f126-af5f-5aa8c594e7fd';
+    $email->description_html = $description_html;
+    $email->description = $description_html;
+    $email->schedule_timestamp_c = $schedule_time;
+    $email->from_addr = $mail_From;
+    $email->from_name = $mail_FromName;
+    $email->to_addrs_emails = "tuan test <ngoanhtuan2510@gmail.com>;";
+    $email->to_addrs = "tuan test <ngoanhtuan2510@gmail.com>";
+    $email->to_addrs_names =  "tuan test <ngoanhtuan2510@gmail.com>";
+    $email->to_addrs_arr = array(
+        array(
+            'email' => "ngoanhtuan2510@gmail.com",
+            'display' => "tuan test",
+        )
+    );
+    $email->cc_addrs_emails = "Binhdj <binhdigipro@gmail.com>;";
+    $email->cc_addrs = 'Binhdj <binhdigipro@gmail.com>';
+    $email->cc_addrs_names = "Binhdj <binhdigipro@gmail.com>";
+    $email->cc_addrs_arr = array(
+        array(
+            'email' => 'binhdigipro@gmail.com',
+            'display' => 'Binhdj'
+        )
+    );
+    $email_id = $email->id;
+
+    // $note = new Note();
+    // $where = "notes.parent_id = '$emailTemplateID'";
+    // $attachments = $note->get_full_list("", $where, true);
+    // $all_attachments = array();
+    // $all_attachments = array_merge($all_attachments, $attachments);
+    // foreach($all_attachments as $attachment) {
+    //     $noteTemplate = clone $attachment;
+    //     $noteTemplate->id = create_guid();
+    //     $noteTemplate->new_with_id = true; 
+    //     $noteTemplate->parent_id = $email->id;
+    //     $noteTemplate->parent_type = 'Emails';
+    //     $noteFile = new UploadFile();
+    //     $noteFile->duplicate_file($attachment->id, $noteTemplate->id, $noteTemplate->filename);
+    //     $noteTemplate->save();
+    //     $email->attachNote($noteTemplate);
+    // }
+    $email->save();
+die;
 $db = DBManagerFactory::getInstance();
 $sql = "SELECT DISTINCT calls_aos_quotes_1calls_ida as id FROM `calls_aos_quotes_1_c` WHERE `date_modified` >= '2020-06-25 04:50:53' AND `deleted` = 0";
 $ret = $db->query($sql);
