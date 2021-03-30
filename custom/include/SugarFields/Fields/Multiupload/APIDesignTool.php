768784865d711e9fadb65dd8c3f1a52acde626ee
pureDev2019 <pureDev2019@gmail.com>
2021-03-30 16:04:16 +0700

    update design  api

--- custom/include/SugarFields/Fields/Multiupload/APIDesignTool.php
+++ custom/include/SugarFields/Fields/Multiupload/APIDesignTool.php
@@ -11,119 +11,34 @@
     $design_json = html_entity_decode($_REQUEST['design_json']);
     $type = $_REQUEST['type'];
     $status = $_REQUEST['status'];
-    $tabActive = $_REQUEST['tabActive'];
-    $dataURL = base64_decode($_REQUEST['dataURL']);
     $quote  = new AOS_Quotes();
     $quote->retrieve($quote_id);
     $quoteType = '';
     $designType ='';
-    if($quote->quote_type_c == 'quote_type_daikin' || $quote->quote_type_c == 'quote_type_nexura'){
-        $quoteType = 'Daikin';
-        if($tabActive == 'floor-plan-option') {
-            $designType = '_FloorPlan';
-        } else if ($tabActive == 'daikin-option-indoor') {
-            $designType = '_Indoor';
-        } else {
-            $designType = '_Outdoor';
-        }
-
-    }else if($quote->quote_type_c == 'quote_type_sanden'){
-        $quoteType = 'Sanden';
-    }
-
-    if(empty($quote->id)) echo json_encode(array());
+        if(empty($quote->id)) echo json_encode(array());
     $dataReturn = [];
 
     if($type == 'save'){
         $quote->design_tool_json_c = $design_json;
         $quote->save();
-        $img = str_replace('data:image/jpeg;base64,', '', $dataURL);
-        $img = str_replace(' ', '+', $img);
-        $data = base64_decode($img);
-        $path = dirname(__FILE__)."/server/php/files/".$quote->pre_install_photos_c;
-        $files = scandir($path,SCANDIR_SORT_DESCENDING);
-        $filename = '';
-        if($status == 'override'){
-            foreach($files as $file){
-                if(is_file($path.'/'.$file) && strpos($file,$quoteType.$designType."_Design_Proposed_Install_Location") !== false){
-                    $filename = explode(".",$file)[0];
-                    $source = $path.'/'.$file;
-                    break;
+
+        if($quote->quote_type_c == 'quote_type_daikin' || $quote->quote_type_c == 'quote_type_nexura'){
+            $dataURL = $_REQUEST['dataURL'];
+            $quoteType = 'Daikin';
+            for($i = 0 ; $i < count($dataURL); $i++){
+                if($i == 0){
+                    $designType = '_FloorPlan';
+                }else{
+                    $designType = '_'.$i;
                 }
+                createImage($quote,base64_decode($dataURL[$i]),$designType,$quoteType,$status);
             }
-        }else{
-            $time_string = strftime("%d%b%Y_%H%M",time());
-            $filename = 'Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string;
-            $source = $path.'/Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string.'.jpeg';
-        }
-
-        if($filename == ''){
-            $time_string = strftime("%d%b%Y_%H%M",time());
-            $filename = 'Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string;
-            $source = $path.'/Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string.'.jpeg';
-        }
-
-
-        
-        $success = file_put_contents($source, $data);
-        if($success !== false){
-            if (exif_imagetype($source) == 2) {
-                $type = 'jpeg';
-                $new_name = $path.'/'.$filename.'.jpg';
-                rename( $source,$new_name);
-            }else if(exif_imagetype($source) == 3){
-                $type = 'png';
-                $new_name = $path.'/'.$filename.'.png';
-                rename( $source,$new_name);
-            }else if(exif_imagetype($source) == 1){
-                $type = 'gif';
-                $new_name = $path.'/'.$filename.'.gif';
-                rename( $source,$filename);
-            } else {
-                return;
-            }
-            if($type == 'gif' || $type == 'jpeg' || $type == 'png') {
-                //create thumbnail
-                if(!file_exists ($path."/thumbnail/")) {
-                    mkdir($path."/thumbnail/");
-                }
-                $typeok = TRUE;
-                $thumb =  $path."/thumbnail/".$filename.'.'.$type;
-                switch ($type) {
-                    case 'jpeg':
-                        $src_func = 'imagecreatefromjpeg';
-                        $write_func = 'imagejpeg';
-                        $thumb =  $path."/thumbnail/".$filename.'.jpg';
-                        $image_quality = isset($options['jpeg_quality']) ?
-                            $options['jpeg_quality'] : 75;
-                        break;
-                    case 'png':
-                        $src_func = 'imagecreatefrompng';
-                        $write_func = 'imagepng';
-                        $image_quality = isset($options['png_quality']) ?
-                            $options['png_quality'] : 9;
-                        break;
-                    case 'gif':
-                        $src_func = 'imagecreatefromgif';
-                        $write_func = 'imagegif';
-                        $image_quality = null;
-                        break;
-                    default: $typeok = FALSE; break;
-                }
-                if($typeok){
-                    list($w, $h) = getimagesize($new_name);
     
-                    $src = $src_func($new_name);
-                    $new_img = imagecreatetruecolor(80,80);
-                    imagecopyresampled($new_img,$src,0,0,0,0,80,80,$w,$h);
-                    $write_func($new_img,$thumb, $image_quality);
-                    
-                    imagedestroy($new_img);
-                    imagedestroy($src);
-                }
-            } 
+        }else if($quote->quote_type_c == 'quote_type_sanden'){
+            $dataURL = base64_decode($_REQUEST['dataURL']);
+            $quoteType = 'Sanden';
+            createImage($quote,$dataURL,$designType,$quoteType,$status );
         }
-        
     }
 
     $dataReturn['design_json'] = html_entity_decode($quote->design_tool_json_c);
@@ -132,4 +47,93 @@
     $dataReturn['quote_number'] = $quote->number;
 
     echo json_encode($dataReturn);
+
+    function createImage($quote,$dataURL,$designType,$quoteType,$status){
+        if($dataURL != ''){
+            $img = preg_replace('/data:image\/(.*?);base64,/', '', $dataURL);
+            $img = str_replace(' ', '+', $img);
+            $data = base64_decode($img);
+            $path = dirname(__FILE__)."/server/php/files/".$quote->pre_install_photos_c;
+            $files = scandir($path,SCANDIR_SORT_DESCENDING);
+            $filename = '';
+            if($status == 'override' || $quoteType == 'Daikin'){
+                foreach($files as $file){
+                    if(is_file($path.'/'.$file) && strpos($file,$quoteType.$designType."_Design_Proposed_Install_Location") !== false){
+                        $filename = explode(".",$file)[0];
+                        $source = $path.'/'.$file;
+                        break;
+                    }
+                }
+            }else{
+                $time_string = strftime("%d%b%Y_%H%M",time());
+                $filename = 'Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string;
+                $source = $path.'/Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string.'.jpeg';
+            }
+
+            if($filename == ''){
+                $time_string = strftime("%d%b%Y_%H%M",time());
+                $filename = 'Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string;
+                $source = $path.'/Q'.$quote->number.'_'.$quoteType.$designType.'_Design_Proposed_Install_Location'.$time_string.'.jpeg';
+            }
+            
+            $success = file_put_contents($source, $data);
+            if($success !== false){
+                if (exif_imagetype($source) == 2) {
+                    $type = 'jpeg';
+                    $new_name = $path.'/'.$filename.'.jpg';
+                    rename( $source,$new_name);
+                }else if(exif_imagetype($source) == 3){
+                    $type = 'png';
+                    $new_name = $path.'/'.$filename.'.png';
+                    rename( $source,$new_name);
+                }else if(exif_imagetype($source) == 1){
+                    $type = 'gif';
+                    $new_name = $path.'/'.$filename.'.gif';
+                    rename( $source,$filename);
+                } else {
+                    return;
+                }
+                if($type == 'gif' || $type == 'jpeg' || $type == 'png') {
+                    //create thumbnail
+                    if(!file_exists ($path."/thumbnail/")) {
+                        mkdir($path."/thumbnail/");
+                    }
+                    $typeok = TRUE;
+                    $thumb =  $path."/thumbnail/".$filename.'.'.$type;
+                    switch ($type) {
+                        case 'jpeg':
+                            $src_func = 'imagecreatefromjpeg';
+                            $write_func = 'imagejpeg';
+                            $thumb =  $path."/thumbnail/".$filename.'.jpg';
+                            $image_quality = isset($options['jpeg_quality']) ?
+                                $options['jpeg_quality'] : 75;
+                            break;
+                        case 'png':
+                            $src_func = 'imagecreatefrompng';
+                            $write_func = 'imagepng';
+                            $image_quality = isset($options['png_quality']) ?
+                                $options['png_quality'] : 9;
+                            break;
+                        case 'gif':
+                            $src_func = 'imagecreatefromgif';
+                            $write_func = 'imagegif';
+                            $image_quality = null;
+                            break;
+                        default: $typeok = FALSE; break;
+                    }
+                    if($typeok){
+                        list($w, $h) = getimagesize($new_name);
+        
+                        $src = $src_func($new_name);
+                        $new_img = imagecreatetruecolor(80,80);
+                        imagecopyresampled($new_img,$src,0,0,0,0,80,80,$w,$h);
+                        $write_func($new_img,$thumb, $image_quality);
+                        
+                        imagedestroy($new_img);
+                        imagedestroy($src);
+                    }
+                } 
+            }
+        }
+    }
 ?>
