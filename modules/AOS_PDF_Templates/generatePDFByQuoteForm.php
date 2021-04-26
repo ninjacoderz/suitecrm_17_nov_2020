7f437935bc655776533a9a792782b95c3eb64b5d
pureDev2019 <pureDev2019@gmail.com>
2021-04-26 14:43:20 +0700

    fix

--- modules/AOS_PDF_Templates/generatePDFByQuoteForm.php
+++ modules/AOS_PDF_Templates/generatePDFByQuoteForm.php
@@ -816,7 +816,7 @@ if ($task == 'pdf' || $task == 'emailpdf') {
                             '4cfa35e8-c49c-6b3c-b6de-5de8b14da844' 
                             // 'a36070a9-e51b-f1a7-8d7e-5d96adaf4300'
                         );
-                        $body = trim(strip_tags(html_entity_decode(parse_sms_template($smsTemplate,$first_name_quote),ENT_QUOTES)));
+                        $body = trim(strip_tags(parse_sms_template($smsTemplate,$first_name_quote)));
                         
                     } elseif($_REQUEST['type_form'] == 'sanden_sms') {
                         $smsTemplate = BeanFactory::getBean(
@@ -824,7 +824,7 @@ if ($task == 'pdf' || $task == 'emailpdf') {
                             '3e22c117-8ecf-597c-550b-5de8b1026b5b' 
                             //'a36070a9-e51b-f1a7-8d7e-5d96adaf4300'
                         );
-                        $body = trim(strip_tags(html_entity_decode(parse_sms_template($smsTemplate,$first_name_quote),ENT_QUOTES)));
+                        $body = trim(strip_tags(parse_sms_template($smsTemplate,$first_name_quote)));
                     }
 
                     $message_dir = '/var/www/message';
@@ -840,7 +840,8 @@ if ($task == 'pdf' || $task == 'emailpdf') {
                     }
 
                     $sms_body = $body.'-'.$admin_name;
-                    
+                    //thienpb fix
+                    $sms_body = preg_replace("/&#?[a-z0-9]{2,8};/i"," ", $sms_bodys);
                     exec("cd ".$message_dir."; php send-message.php sms ".$phone_number." ".escapeshellarg($sms_body));
 
                     if($_REQUEST['type_form'] == 'sanden_sms') {
