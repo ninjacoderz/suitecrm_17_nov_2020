65c1dfd412a455f6e2dbd6125c604f1f2bcfd982
pureDev2019 <pureDev2019@gmail.com>
2021-05-21 16:29:55 +0700

    PVWatts

--- custom/modules/AOS_Quotes/APIrederectToPVwatts.php
+++ custom/modules/AOS_Quotes/APIrederectToPVwatts.php
@@ -1,7 +1,7 @@
 <?php
     $address= str_replace(' ', '%20', $_REQUEST['mylocation']);
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://pvwatts.nrel.gov/handle_mylocation.php?myloc='.$address);
+    curl_setopt($ch, CURLOPT_URL, 'https://pvwatts.nrel.gov/handle_mylocation.php?myloc=');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -22,7 +22,7 @@
     $headers[] = 'Sec-Fetch-Dest: empty';
     $headers[] = 'Referer: https://pvwatts.nrel.gov/pvwatts.php';
     $headers[] = 'Accept-Language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5';
-    $headers[] = 'Cookie: nrelGovGA=GA1.2.493334013.1619140484; DAV_PVWATTS=GA1.2.493334013.1619140484; _ga=GA1.2.493334013.1619140484; _ga=GA1.3.493334013.1619140484; PHPSESSID=9acf8316c196aeb4ca60c551cdde212e; nrelGovGA_gid=GA1.2.1588324627.1619402695; DAV_PVWATTS_gid=GA1.2.1881014456.1619402695; _gid=GA1.2.988322776.1619402696; _gid=GA1.3.988322776.1619402696; _gat_UA-121046212-1=1; _gat_nrelGovTracker=1; _gat_DAV_PVWATTS=1';
+    $headers[] = 'Cookie: nrelGovGA=GA1.2.493334013.1619140484; DAV_PVWATTS=GA1.2.493334013.1619140484; _ga=GA1.2.493334013.1619140484; _ga=GA1.3.493334013.1619140484; PHPSESSID=2e7f44d82f6edb7b286987cc3f3dc1b1; nrelGovGA_gid=GA1.2.1588324627.1619402695; DAV_PVWATTS_gid=GA1.2.1881014456.1619402695; _gid=GA1.2.988322776.1619402696; _gid=GA1.3.988322776.1619402696; _gat_UA-121046212-1=1; _gat_nrelGovTracker=1; _gat_DAV_PVWATTS=1';
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     
     $result = curl_exec($ch);
