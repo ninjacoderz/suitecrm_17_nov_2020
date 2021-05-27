8eb32f00fab68d9f9caf78876b9f82134aede718
pureDev2019 <pureDev2019@gmail.com>
2021-05-27 16:07:28 +0700

    fix

--- custom/modules/pe_product_prices/createProductPrices.php
+++ custom/modules/pe_product_prices/createProductPrices.php
@@ -2,8 +2,8 @@
 
 $parent_id = $_REQUEST['parent_id'];
 // $parent_module = $_REQUEST['parent_module'];
-
-$product =  BeanFactory::getBean("AOS_Products", $parent_id);
+$product = new AOS_Products();
+$product->retrieve($parent_id);
 if ($product->id) {
     $price = new pe_product_prices();
     $price->name = $product->name;
