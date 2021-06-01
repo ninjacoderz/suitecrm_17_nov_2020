adf811006cab231243837aa37b29b5314a4f743d
pureDev2019 <pureDev2019@gmail.com>
2021-06-01 08:18:16 +0700

    fix sort subpanel > https://trello.com/c/OdZBWCVk/3361-product-price-sub-module-for-products-sort-by-cost-the-product-prices-disappear

--- modules/AOS_Products/customSubpanelFunction.php
+++ modules/AOS_Products/customSubpanelFunction.php
@@ -1,11 +1,13 @@
 <?php
 
 function get_product_prices() {
-    $args = func_get_args();
-    $id = $args[0]['product_id'];
+    global $app;
+    $controller = $app->controller;
+    $bean = $controller->bean;
+
     $return_array['select']='SELECT DISTINCT pe_product_prices.id as prices_id';
     $return_array['from']='FROM pe_product_prices';
-    $return_array['where']="pe_product_prices.deleted = 0 AND aos_products.id ='{$id}'";
+    $return_array['where']="pe_product_prices.deleted = 0 AND aos_products.id ='{$bean->id}'";
     $return_array['join'] = "LEFT JOIN aos_products ON aos_products.part_number = pe_product_prices.part_number";
     $return_array['join_tables'][0] = '';
     return $return_array;
