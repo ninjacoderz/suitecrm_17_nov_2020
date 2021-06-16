cccce754bfd7f198af67b13245af22969e354013
pureDev2019 <pureDev2019@gmail.com>
2021-06-16 15:12:43 +0700

    comment sql update number pricing opt

--- custom/modules/pe_service_case/readGoogleSheet.php
+++ custom/modules/pe_service_case/readGoogleSheet.php
@@ -1,7 +1,7 @@
 <?php
 
-$db = DBManagerFactory::getInstance();
-$sql_set_count = "SET @count = 0;";
-$result = $db->query($sql_set_count);
-$sql_number = "UPDATE pe_pricing_options SET number = @count:=@count+1 ORDER BY date_entered ASC";
-$result = $db->query($sql_number);
+// $db = DBManagerFactory::getInstance();
+// $sql_set_count = "SET @count = 0;";
+// $result = $db->query($sql_set_count);
+// $sql_number = "UPDATE pe_pricing_options SET number = @count:=@count+1 ORDER BY date_entered ASC";
+// $result = $db->query($sql_number);
